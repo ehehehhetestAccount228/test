f12078a0e2e4900e78bc9afae8f54d6a610fe7f7
marataziat <marataziat@gmail.com>
2018-11-02 05:29:00 +0600

    Color feature?

--- app.js
+++ app.js
@@ -3,13 +3,30 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var clients = io.sockets.clients();
 
+function hashCode(str) { // java String#hashCode
+    var hash = 0;
+    for (var i = 0; i < str.length; i++) {
+       hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    return hash;
+} 
+
+function intToRGB(i){
+    var c = (i & 0x00FFFFFF)
+        .toString(16)
+        .toUpperCase();
+
+    return "00000".substring(0, 6 - c.length) + c;
+}
+
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+    var address = socket.handshake.address;
     socket.on('pointers', function(msg){
-        socket.broadcast.emit('pointers', msg);
+        socket.broadcast.emit('pointers', {x:msg.x,y:msg.y,hex:intToRGB(hashCode(address))});
     });
 });
 
