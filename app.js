dcbd14667d7268696524352137dd34e8a9dc327a
marataziat <marataziat@gmail.com>
2018-11-02 13:32:17 +0600

    Prod :)

--- app.js
+++ app.js
@@ -3,30 +3,12 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var clients = io.sockets.clients();
 
-// String to color function
-function hashCode(str) { // java String#hashCode
-    var hash = 0;
-    for (var i = 0; i < str.length; i++) {
-       hash = str.charCodeAt(i) + ((hash << 5) - hash);
-    }
-    return hash;
-} 
-
-function intToRGB(i){
-    var c = (i & 0x00FFFFFF)
-        .toString(16)
-        .toUpperCase();
-
-    return "00000".substring(0, 6 - c.length) + c;
-}
-// END
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
     socket.on('pointers', function(msg){
-        console.log(socket.id)
         socket.send(socket.id);
         socket.broadcast.emit('pointers', {x:msg.x,y:msg.y,hex:socket.id});
     });
