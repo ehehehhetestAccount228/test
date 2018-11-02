a08d45b55fb2b89313ba20ca9439b30212367829
marataziat <marataziat@gmail.com>
2018-11-02 18:53:38 +0600

    Anti flooding feature #4

--- app.js
+++ app.js
@@ -2,7 +2,17 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var clients = io.sockets.clients();
+var { RateLimiterMemory } = require('rate-limiter-flexible');
+ipfilter = require('ipfilter')
+var ips = [];
 
+const rateLimiter = new RateLimiterMemory(
+{
+      points: 65, // 5 points
+      duration: 1, // per second
+});
+
+app.use(ipfilter(ips, {mode: 'block'}));
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
@@ -11,8 +21,12 @@ io.on('connection', function(socket){
     socket.broadcast.emit('users', {count: io.engine.clientsCount});
 
     socket.on('pointers', function(msg){
-        socket.send(socket.id);
-        socket.broadcast.emit('pointers', {x:msg.x,y:msg.y,hex:socket.id});
+        rateLimiter.consume(socket.handshake.address).then(() => {
+            socket.broadcast.emit('pointers', {x:msg.x,y:msg.y,hex:socket.id});
+        }).catch((rejRes) => {
+            // When rate limitations is exteed ban IP (add to banned IP array). Yes. just Permanent. ban.!
+            ips.push(socket.handshake.address)
+          });
     });
     socket.on('disconnect', function(msg) {
         socket.broadcast.emit('users', {count: io.engine.clientsCount});
