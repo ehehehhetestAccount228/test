5df8e09fdfb7dabe1f4460629e996829d7e39172
marataziat <marataziat@gmail.com>
2018-11-04 23:11:38 +0600

    #11 Add logging

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+const log = require('simple-node-logger').createSimpleFileLogger('game.log');
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
@@ -16,14 +17,18 @@ app.get('/beep.mp3', (req,res)=>{
 
 io.on('connection', function (socket) {
     socket.broadcast.emit('users', { count: io.sockets.server.eio.clientsCount });
+    log.info(`New user ${socket.id} has been connected`);
 
+    // if I add rate limiting, please add logs for rate limiting fails too! (log rate limitting must be also with rate limiting)
     socket.on('pointers', function (msg) {
         socket.broadcast.emit('pointers', { x: msg.x, y: msg.y, hex: socket.id });
     });
     socket.on('alert', function (msg) {
+        log.info(`User ${socket.id} did the Beep action`);
         socket.broadcast.emit('alert', { });
     });
     socket.on('disconnect', function (msg) {
+        log.info(`User ${socket.id} has been disconnected`);
         socket.broadcast.emit('users', { count: io.sockets.server.eio.clientsCount });
     });
 });
