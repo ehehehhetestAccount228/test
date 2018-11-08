a753439939e32b06c9fee348d85d3faa200fb1dc
eternalwaveform <42590846+eternalwaveform@users.noreply.github.com>
2018-11-08 11:58:11 +0300

    Add rate limiting, optimize network usage and add mute feature

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const log = require('simple-node-logger').createSimpleFileLogger('game.log');
+const { RateLimiterMemory } = require('rate-limiter-flexible');
+
+const rateLimiter = new RateLimiterMemory({
+    points: 4,
+    duration: 1
+});
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -11,18 +17,33 @@ app.get('/beep.mp3', (req, res) => {
     res.sendFile(__dirname + '/beep.mp3');
 });
 
+let points = [];
+let now = Date.now();
+setInterval(() => {
+    if (points.length > 0) {
+        io.emit('points', points);
+        points = [];
+    }
+}, 500);
+
 io.on('connection', (socket) => {
     io.emit('users', { count: io.sockets.server.eio.clientsCount });
     log.info(`User ${socket.id} connected`);
 
-    // if I add rate limiting, please add logs for rate limiting fails too! (log rate limiting must be also with rate limiting)
-    socket.on('point', (msg) => {
-        socket.broadcast.emit('point', { x: msg.x, y: msg.y, id: socket.id });
+    socket.on('points', (data) => {
+        rateLimiter.consume(socket.id).then(() => {
+            for (let point of data) {
+                if (point.delay > 500) point.delay = 500;
+                points.push({ x: point.x, y: point.y, id: socket.id, delay: point.delay });
+            }
+        }).catch(() => {
+            log.info(`User ${socket.id} exceeded rate limit`)
+        });
     });
 
     socket.on('beep', (msg) => {
         log.info(`User ${socket.id} sent "beep" action`);
-        socket.broadcast.emit('alert', { });
+        socket.broadcast.emit('beep', { });
     });
 
     socket.on('disconnect', (msg) => {
