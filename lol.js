362b6d707b9b8dda0a3e0d1e430a48919cbd5ec4
marataziat <marataziat@gmail.com>
2018-12-30 19:02:54 +0600

    Delete trash :)

--- lol.js
+++
@@ -1,4 +0,0 @@
-var imgur = require('imgur');
-imgur.uploadFile('test.png', 'iXUbfpc').then((json)=>{console.log(json.data.link)}).catch(function (err) {
-        console.error(err.message);
-    });
